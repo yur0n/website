c328b5993cce6229d2046196a982c09d482e0ee2
yur0n <86810041+yur0n@users.noreply.github.com>
2023-12-23 02:50:33 +0200

    Update index.js
    new node.js feature import.meta.dirname added

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import path from 'path';
 import socket from './socket.js';
 const port = process.env.PORT || 3000;
 const portTls = process.env.PORT_TLS || 443;
-const __dirname = path.resolve();
 // Certificate
-const privateKey = fs.readFileSync(path.join(__dirname,'../certs/private.key.pem'), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, '../certs/domain.cert.pem'), 'utf8');
-//const ca = fs.readFileSync(path.join(__dirname, '../certs/chain1.pem'), 'utf8');
+const privateKey = fs.readFileSync(path.join(import.meta.dirname, '../certs/private.key.pem'), 'utf8');
+const certificate = fs.readFileSync(path.join(import.meta.dirname, '../certs/domain.cert.pem'), 'utf8');
+//const ca = fs.readFileSync(path.join(import.meta.dirname, '../certs/chain1.pem'), 'utf8');
 const credentials = {
 	key: privateKey,
 	cert: certificate,
