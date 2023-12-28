3ef35cf6b128674aa0fd886d52cc1a8f696d0b72
yur0n <86810041+yur0n@users.noreply.github.com>
2023-12-28 10:07:50 +0200

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,12 +2,9 @@ import mongoose from 'mongoose'
 const login = process.env.MONGODB_LOGIN
 const pass = process.env.MONGODB_PASS
 
-const connection = mongoose.connect(`mongodb+srv://${login}:${pass}@cluster0.0na8y.mongodb.net/Website?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${login}:${pass}@cluster0.0na8y.mongodb.net/Website?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     //useCreateIndex: true,
     //useFindAndModify: false
 })
-
-export const websiteDb = connection.useDb('Website');
-export const telegramDb = connection.useDb('telegram');
