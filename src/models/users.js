649f3896dba28eae7f117406b8a8061190ddc8e9
yur0n <86810041+yur0n@users.noreply.github.com>
2024-01-09 17:03:15 +0200

    up

--- src/models/users.js
+++ src/models/users.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose"
 // export default mongoose.connection.db.collection(
 // 	"users",
 // );
-
+mongoose.set('strictQuery', true)
 const schema = new mongoose.Schema({
     key: 'string',
-    data: {},
+    value: {},
 })
 
 export default mongoose.model('User', schema)
