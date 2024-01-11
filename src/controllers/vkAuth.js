7bd16693c5f6e4cecdf8d2794813b0883d089df8
yur0n <86810041+yur0n@users.noreply.github.com>
2024-01-12 00:29:19 +0200

    up

--- src/controllers/vkAuth.js
+++ src/controllers/vkAuth.js
@@ -1,21 +1,22 @@
 import axios from 'axios'
-import Session from '../models/sessions.js'
+import User from '../models/users.js'
 
 export default async (req, res) => {
-    axios.get(process.env.VK_REDIRECT_LINK + req.query.code)
-    .then(async(data) => {
-        
-        console.log(data.data.user_id, req.query.state)
-
-        const user = await Session.findOne({key: req.query.state}).catch(console.log)
-	    if (!user) return res.send('Authorized but your token is not connected to Get Posts, write to support')
-        user.data.access_tokenVK = data.data.access_token
-	    user.markModified('data')
-        await user.save().catch(console.log)
-
-        res.send('Authorized, you can close the page now') 
+	const code = req.query.code
+	const state = req.query.state
+	if (!code || !state) return res.send('No data to authenticate')
+    axios.get(process.env.VK_REDIRECT_LINK + code)
+    .then(async data => {
+        const user = await User.findOne({ key: state })
+	    if (!user) {
+			return res.send('Your token is not connected to Get Posts, write to support')
+		}
+        user.value.auths.vkAuth = data.data.access_token
+	    user.markModified('value')
+        await user.save()
+        res.send('Authenticated, you can close the page now') 
     }).catch((e) => {
         console.log(e)
-        res.send('Not authorized, something went wrong') 
+        res.send('Not authenticated, something went wrong') 
     })
 }
