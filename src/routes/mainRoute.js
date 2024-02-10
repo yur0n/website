ed2904163f80c1e1df322e43e75ba45dca7abf2b
yur0n <yur0n@yahoo.com>
2024-02-10 19:51:02 +0200

    up

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -20,10 +20,12 @@ router.get('/player', player)
 router.get('/ip', checkIP)
 
 router.get('/getposts', (req, res) => {
+    console.log(req.body, req.query)
     res.sendFile(path.join(import.meta.dirname, '../../templates/views/getposts.html'))
 })
 
 router.post('/getposts', (req, res) => {
+    console.log(req.body, req.query)
     const {name, email, message} = req.body
     if (!name || !email || !message) return res.status(400).send('Complete all fields!')
     res.send('Message recived. Thank you!')
