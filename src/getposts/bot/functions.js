4f6754a20259df95476098c7bd17f194ad179656
yur0n <86810041+yur0n@users.noreply.github.com>
2024-03-06 21:58:36 +0200

    Update functions.js

--- src/getposts/bot/functions.js
+++ src/getposts/bot/functions.js
@@ -7,20 +7,18 @@ export async function deleteMsg(ctx, chat, msg) {
 }
 
 export async function deleteMsgTime(ctx, chat, msg, time = 2500) {
-	setTimeout(async () => {
-		try {
-			await ctx.api.deleteMessage(chat, msg);
-		} catch (e) {}
-	}, time);
+	await new Promise(resolve => setTimeout(resolve, time))
+	try {
+		await ctx.api.deleteMessage(chat, msg)
+	} catch (e) {}
 }
 
 export async function replyAndDel(ctx, text, time = 2500) {
-	const msg = await ctx.reply(text)
-	setTimeout(async () => {
-		try {
-			await ctx.api.deleteMessage(msg.chat.id, msg.message_id)
-		} catch (e) {}
-	}, time)
+	await new Promise(resolve => setTimeout(resolve, time))
+	try {
+		const msg = await ctx.reply(text)
+		await ctx.api.deleteMessage(msg.chat.id, msg.message_id)
+	} catch (e) {}
 }
 
 export async function isAdmin(ctx, chat_id) {
