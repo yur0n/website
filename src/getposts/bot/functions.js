3b36f43c42a8dea4f4f529f8f78c930b8f0e09fd
yur0n <86810041+yur0n@users.noreply.github.com>
2024-03-14 14:55:00 +0200

    Update functions.js
    Updated reply and delete function

--- src/getposts/bot/functions.js
+++ src/getposts/bot/functions.js
@@ -14,9 +14,9 @@ export async function deleteMsgTime(ctx, chat, msg, time = 2500) {
 }
 
 export async function replyAndDel(ctx, text, time = 2500) {
+	const msg = await ctx.reply(text)
 	await new Promise(resolve => setTimeout(resolve, time))
 	try {
-		const msg = await ctx.reply(text)
 		await ctx.api.deleteMessage(msg.chat.id, msg.message_id)
 	} catch (e) {}
 }
