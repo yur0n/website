1e33332a460fe7e957cafe85e93e6b4e2db7068f
yur0n <86810041+yur0n@users.noreply.github.com>
2024-03-14 19:09:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ import socket from './socket.js';
 const port = process.env.PORT || 3000;
 const portTls = process.env.PORT_TLS || 443;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log('Server is up on port: ' + port);
 })
 
-// Certs
-const privateKey = fs.readFileSync(path.join(import.meta.dirname, '../certs/cdn_private.key.pem'), 'utf8');
-const certificate = fs.readFileSync(path.join(import.meta.dirname, '../certs/cdn_domain.cert.pem'), 'utf8');
-// const ca = fs.readFileSync(path.join(import.meta.dirname, '../certs/authenticated_origin_pull_ca.pem'), 'utf8');
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	// ca: ca
-};
-const httpsServer = https.createServer(credentials, app);
+// // Certs
+// const privateKey = fs.readFileSync(path.join(import.meta.dirname, '../certs/cdn_private.key.pem'), 'utf8');
+// const certificate = fs.readFileSync(path.join(import.meta.dirname, '../certs/cdn_domain.cert.pem'), 'utf8');
+// // const ca = fs.readFileSync(path.join(import.meta.dirname, '../certs/authenticated_origin_pull_ca.pem'), 'utf8');
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	// ca: ca
+// };
+// const httpsServer = https.createServer(credentials, app);
 
-const server = httpsServer.listen(portTls, () => {
-	console.log('HTTPS Server running on port: ' + portTls);
-});
+// const server = httpsServer.listen(portTls, () => {
+// 	console.log('HTTPS Server running on port: ' + portTls);
+// });
 
 socket(server);
