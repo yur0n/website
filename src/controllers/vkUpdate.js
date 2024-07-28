12af34a6666769c4eb36270815e72a0d07725524
yur0n <86810041+yur0n@users.noreply.github.com>
2024-07-29 01:08:32 +0300

    up

--- src/controllers/vkUpdate.js
+++ src/controllers/vkUpdate.js
@@ -62,7 +62,6 @@ const inlineKeyboard = (url1, url2) => {        // inline keyboard as object of 
 
 export default async (req, res) => {
     const data = req.body;
-    console.log(data)
     try {
         const restricted = [441232274] // const restricted = [441232274, ...Object.values(ids).map(id => '-' + id)]
         if (restricted.includes(data.object?.from_id)) return
